2e99f73703ad7357272308169c630a3636cbe6ba
Md. Neamat Ullah <mdneamat81@gmail.com>
2024-10-31 13:23:29 +0600

    notice popup

--- resources/views/frontend/notice/notice.blade.php
+++ resources/views/frontend/notice/notice.blade.php
@@ -76,7 +76,7 @@
                 <div class="row g-4">
                     @foreach($notices as $notice)
                     <div class="col-md-6 col-lg-4">
-                        <div class="notice-content text-center">
+                        <div class="notice-content text-center" onclick="showModal('{{ static_asset($notice->image) }}', '{{ $notice->title }}')">
                             <div class="notice-title">
                                 <h3>{!! $notice->title !!}</h3>
                             </div>
@@ -92,7 +92,30 @@
             </div>
         </div>
     </section>
-
 </main>
-@endsection
+<!-- Modal Structure -->
+<div id="imageModal" class="modal fade" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 id="imageModalLabel" class="modal-title"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+                <img id="modalImage" src="" class="img-fluid" alt="">
+            </div>
+        </div>
+    </div>
+</div>
 
+@endsection
+@push('script')
+    <script>
+        function showModal(imageSrc, title) {
+            document.getElementById('modalImage').src = imageSrc;
+            document.getElementById('imageModalLabel').innerText = title;
+            var modal = new bootstrap.Modal(document.getElementById('imageModal'));
+            modal.show();
+        }
+    </script>
+@endpush
