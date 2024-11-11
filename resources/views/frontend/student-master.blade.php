b9a45d7e556d80aa016fa22cdab7d3d8dd22c951
Imrul00729 <imrulhasan303@gmail.com>
2024-11-11 21:36:13 +0600

    Added responsive styles for better layout on smaller screens and adjusted column widths in various templates for improved design coherence.

--- resources/views/frontend/student-master.blade.php
+++ resources/views/frontend/student-master.blade.php
@@ -178,7 +178,7 @@
 <section>
     <div class="container-fluid">
         <div class="row custom_col_res py-lg-5">
-            <div class="col-md-2 stu_btn">
+            <div class="col-md-3 col-lg-2 stu_btn">
                 <!-- <div class="bg-dark pt-5 mobile_res" style="min-height: 450px; height: 100%"> -->
                 <div class="bg-light mobile_res">
                     <ul class="nav flex-column student-panel-menu shadow-sm">
